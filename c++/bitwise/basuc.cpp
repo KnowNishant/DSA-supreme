7777e4e8c52da7061398fc85010a340877d00827
Nishant <Nishantkr20052@gmail.com>
2024-06-15 10:14:20 +0530

    new change

--- c++/bitwise/basuc.cpp
+++ c++/bitwise/basuc.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 int main()
 {
-    
+    int length;
+    cout << "enter the length: ";
+    cin >> length;
+    int i,j;
+    for(i=0;i<length;i++){
+        for(j=0;j<i+1;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
     return 0;
 
 }
