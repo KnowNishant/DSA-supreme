74216dd1e5436ae05885202a3f7067becec3bbaf
Nishant <Nishantkr20052@gmail.com>
2024-08-07 19:16:52 +0530

    practice

---
+++ c++/2d arrays/join_Twoarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[6]={1,2,3,4,5,6};
+    int sizea=6;
+    int b[5]={4,7,8,2,1};
+    int sizeb=5;
+    int c[20];
+
+    for(int i=0;i<(sizea);i++){
+        c[i]=a[i];
+    }
+    for(int i=sizea;i<(sizeb+sizea);i++){
+        c[i]=b[(i-sizea)];
+    }
+    for(int i=0;i<(sizeb+sizea);i++){
+        cout << " "<<c[i];
+    }
+    return 0;
+}
